68f5051255332c282da70318ae842ac69f202a4c
vashenkoda <d.vaschenko@haulmont.net>
2025-04-01 19:16:25 +0300

    LDAP with JPA User sync

--- src/main/java/com/company/jmixpmsecurity/app/RegistrationCleaner.java
+++ src/main/java/com/company/jmixpmsecurity/app/RegistrationCleaner.java
@@ -4,7 +4,12 @@ import com.company.jmixpmsecurity.entity.User;
 import io.jmix.core.DataManager;
 import io.jmix.core.DateTimeTransformations;
 import io.jmix.core.TimeSource;
+import io.jmix.core.UnconstrainedDataManager;
+import io.jmix.core.security.SystemAuthenticator;
 import org.apache.commons.lang3.time.DateUtils;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,33 +17,41 @@ import java.time.OffsetDateTime;
 import java.util.Date;
 import java.util.List;
 
-public class RegistrationCleaner {
+public class RegistrationCleaner implements Job {
     private static final Logger log = LoggerFactory.getLogger(RegistrationCleaner.class);
 
-    private final DataManager dataManager;
+    private final UnconstrainedDataManager unconstrainedDataManager;
     private final TimeSource timeSource;
     private final DateTimeTransformations dateTimeTransformations;
+    private final SystemAuthenticator systemAuthenticator;
 
-    public RegistrationCleaner(DataManager dataManager,
+    public RegistrationCleaner(UnconstrainedDataManager unconstrainedDataManager,
                                TimeSource timeSource,
-                               DateTimeTransformations dateTimeTransformations) {
-        this.dataManager = dataManager;
+                               DateTimeTransformations dateTimeTransformations, SystemAuthenticator systemAuthenticator) {
+        this.unconstrainedDataManager = unconstrainedDataManager;
         this.timeSource = timeSource;
         this.dateTimeTransformations = dateTimeTransformations;
+        this.systemAuthenticator = systemAuthenticator;
     }
 
     public String deleteOldNotActivatedUsers() {
         Date lastWeekDate = DateUtils.addDays(timeSource.currentTimestamp(), -7);
         Object threshold = dateTimeTransformations.transformToType(lastWeekDate, OffsetDateTime.class, null);
 
-        List<User> oldUsers = dataManager.load(User.class)
+        List<User> oldUsers = unconstrainedDataManager.load(User.class)
                 .query("select u from User u where u.createdDate < :threshold and u.needsActivation = true")
                 .parameter("threshold", threshold)
                 .list();
         for (User u: oldUsers) {
-            dataManager.remove(u);
+            unconstrainedDataManager.remove(u);
             log.info("Removing old not activated user {}", u.getUsername());
         }
         return "Deleted " + oldUsers.size() + " users";
+    }
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        String result = deleteOldNotActivatedUsers();
+        log.info("Scheduler result: {}", result);
     }
 }
